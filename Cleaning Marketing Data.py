6add95470391499ddfbe3e5bbf7d15fc4faf7986
Vidya Adnina <vidya.adnina@gmail.com>
2024-10-01 09:51:03 +0700

    Update Cleaning Marketing Data.py

--- Cleaning Marketing Data.py
+++ Cleaning Marketing Data.py
@@ -7,14 +7,14 @@ data = pd.read_csv("bank_marketing.csv")
 data['education'] = data['education'].str.replace('.','_')
 data['education'] = data['education'].replace('unknown',np.NaN)
 data['job'] = data['job'].str.replace('.','_')
-data['credit_default'] = data['credit_default'].map({'yes': 1,'no':0,'unknown':0}).astype(bool)
-data['mortgage'] = data['mortgage'].map({'yes': 1,'no':0,'unknown':0}).astype(bool)
+data['credit_default'] = data['credit_default'].map({'yes': 1, 'no':0,'unknown':0}).astype(bool)
+data['mortgage'] = data['mortgage'].map({'yes': 1, 'no':0,'unknown':0}).astype(bool)
 data['campaign_outcome'] = data['campaign_outcome'].map({'no': 0, 'yes': 1}).astype(bool)
-data['previous_outcome'] = data['previous_outcome'].map({'success':1,'failure':0,'nonexistent':0}).astype(bool)
+data['previous_outcome'] = data['previous_outcome'].map({'success':1, 'failure':0, 'nonexistent':0}).astype(bool)
 
-client = data[['client_id','age','job','marital','education','credit_default','mortgage']].copy()
+client = data[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
 
-campaign = data[['client_id','number_contacts','contact_duration','previous_campaign_contacts','previous_outcome','campaign_outcome']].copy()
+campaign = data[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome']].copy()
 
 data['month'] = data['month'].str.capitalize()
 month_map = {
@@ -23,7 +23,7 @@ month_map = {
 }
 data['month'] = data['month'].map(month_map)
 
-campaign['last_contact_date'] = pd.to_datetime('2022-'+data['month'].astype(str) + '-'+data['day'].astype(str), format="%Y-%m-%d")
+campaign['last_contact_date'] = pd.to_datetime('2022-' + data['month'].astype(str) + '-'+data['day'].astype(str), format="%Y-%m-%d")
 
 economics = data[["client_id","cons_price_idx","euribor_three_months"]].copy()
 
